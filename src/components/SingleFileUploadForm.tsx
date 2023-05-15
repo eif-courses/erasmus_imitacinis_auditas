8e9095116ebfa79077ed8a89d146c1cee8cc8a70
Marius <m.gzegozevskis@gmail.com>
2023-05-15 18:37:04 +0300

    Local database test

--- src/components/SingleFileUploadForm.tsx
+++ src/components/SingleFileUploadForm.tsx
@@ -1,25 +1,47 @@
-import { UploadButton } from "@uploadthing/react";
-
+import { UploadButton, UploadDropzone} from "@uploadthing/react";
 import type { OurFileRouter } from "~/server/uploadthing";
+import { Inter } from "next/font/google";
 
+const inter = Inter({ subsets: ["latin"] });
 // You need to import our styles for the button to look right. Best to import in the root /_app.tsx but this is fine
 import "@uploadthing/react/styles.css";
 
 export default function UploadForm() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <UploadButton<OurFileRouter>
-                endpoint="imageUploader"
-                onClientUploadComplete={(res) => {
-                    // Do something with the response
-                    console.log("Files: ", res);
-                    alert("Upload Completed");
-                }}
-                onUploadError={(error: Error) => {
-                    // Do something with the error.
-                    alert(`ERROR! ${error.message}`);
-                }}
-            />
+            <div className="flex flex-col items-center justify-center gap-4">
+        <span className="text-4xl font-bold text-center">
+          {`Upload a file using a button:`}
+        </span>
+
+                <UploadButton<OurFileRouter>
+                    endpoint="imageUploader"
+                    onClientUploadComplete={(res) => {
+                        // Do something with the response
+                        console.log("Files: ", res);
+                        alert("Upload Completed");
+                    }}
+                    onUploadError={(error: Error) => {
+                        alert(`ERROR! ${error.message}`);
+                    }}
+                />
+            </div>
+            <div className="flex flex-col items-center justify-center gap-4">
+        <span className="text-4xl font-bold text-center">
+          {`...or using a dropzone:`}
+        </span>
+                <UploadDropzone<OurFileRouter>
+                    endpoint="imageUploader"
+                    onClientUploadComplete={(res) => {
+                        // Do something with the response
+                        console.log("Files: ", res);
+                        alert("Upload Completed");
+                    }}
+                    onUploadError={(error: Error) => {
+                        alert(`ERROR! ${error.message}`);
+                    }}
+                />
+            </div>
         </main>
     );
 }
--- src/server/uploadthing.ts
+++ src/server/uploadthing.ts
@@ -2,31 +2,28 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next-legacy";
 import {NextApiRequest, NextApiResponse} from "next/types";
 const f = createUploadthing();
-
-const auth = (req: NextApiRequest, res: NextApiResponse) => {
-    return ({id: "fakeId"});
-}; // Fake auth function
-
+import { getServerSession } from "next-auth";
+import { authOptions } from "~/server/auth";
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
     // Define as many FileRoutes as you like, each with a unique routeSlug
     imageUploader: f
         // Set permissions and file types for this FileRoute
-        .fileTypes(["image", "video"])
-        .maxSize("1GB")
+        .fileTypes(["image", "video", "audio", "blob"])
+        .maxSize("16MB")
         .middleware(async (req, res) => {
             // This code runs on your server before upload
-            const user = await auth(req, res);
+            const user = await getServerSession(req, res, authOptions)
 
             // If you throw, the user will not be able to upload
             if (!user) throw new Error("Unauthorized");
 
             // Whatever is returned here is accessible in onUploadComplete as `metadata`
-            return { userId: user.id };
+            return { userId: user.user };
         })
         .onUploadComplete(async ({ metadata, file }) => {
             // This code RUNS ON YOUR SERVER after upload
-            console.log("Upload complete for userId:", metadata.userId);
+            console.log("Upload complete for userId:", metadata.userId.id);
 
             console.log("file url", file.url);
         }),
